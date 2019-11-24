f9e0b59b933fd1bf07e6637dd4bd645b84663358
denisciumac1 <43856379+denisciumac1@users.noreply.github.com>
2019-11-24 15:59:03 +0200

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const color = require('./color')
-const fs      = require("fs")
-const http   = require("http")
+const fs = require("fs")
+const http = require("http")
 // color.getPalette("102-5000x5000.jpg")
 //      .then((palette) => {   // 1.convert to JSON
 //       console.log(palette)
@@ -19,18 +19,32 @@ const http   = require("http")
 //         console.log("DONE!")
 //      })
 
-const server = http.createServer((req, res) =>{
-  if(req.url =="/image" ){
-    var data  = fs.readFileSync("../public/images/102-300x300.jpg")
-    res.write(data) 
-    res.end()
-  }else if(req.url =="/data"){
-    var data  = fs.readFileSync("../public/images_data/102-300x300.jpg.json")
-    res.end(data)
-  }else{
-  res.end("ALL GOOD!")
-  }
-  
-})
+const server = http.createServer((req, res) => {
+      if (result = req.url.match(/\/image\/(.*\.jpg)/)) {
+        try {
+          var data = fs.readFileSync(`../public/images/${result[1]}`)
+          res.write(data)
+        } catch (err) {
+          res.end("Not Found")
+        }
+      } else if (result = req.url.match(/\/data\/(.*\.json)/)) {
+        try {
+          var data = fs.readFileSync(`../public/images_data/${result[1]}`)
+          res.end(data)
+        } catch (err) {
+          res.end("Not Found")
+        }
+      } else if (req.url == "/") {
+          try {
+            var data = fs.readFileSync("../public/index.html")
+            res.write(data)
+          } catch (err) {
+            res.end("Not Found")
+          }
+        } else {
+          res.end("ALL GOOD!")
+        }
 
-server.listen(3000);
+      })
+
+    server.listen(3000);
